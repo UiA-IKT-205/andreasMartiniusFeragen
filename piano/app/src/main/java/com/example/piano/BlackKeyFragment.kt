0d8ef68571982d689a0ab1e4ebec98af0e4d604f
Martinius Feragen <andrmf18@uia.no>
2021-03-02 18:39:38 +0100

    Changed setOnTouchListener on white and black so SuppressLint is not longer needed, note in BlackKeyFragment is now nonnullable, removed unessasery null checks related to that

--- piano/app/src/main/java/com/example/piano/BlackKeyFragment.kt
+++ piano/app/src/main/java/com/example/piano/BlackKeyFragment.kt
@@ -9,11 +9,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.piano.databinding.FragmentBlackKeyBinding
 import kotlinx.android.synthetic.main.fragment_black_key.view.*
+import kotlinx.android.synthetic.main.fragment_white_key.view.*
 
 class BlackKeyFragment : Fragment() {
     private lateinit var _binding: FragmentBlackKeyBinding
     private val binding get() = _binding
-    private var note: String? = null
+    private lateinit var note: String
 
     private val onKeyUp: (note:String) -> Unit = { println("Piano key Up $it") }
     private val onKeyDown: (note:String) -> Unit = { println("Piano key down $it") }
@@ -21,33 +22,28 @@ class BlackKeyFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            if (it.getString("NOTE") == null || it.getString("NOTE") == "?") {
-                note = null
-            } else {
-                note = it.getString("NOTE")!!
-            }
+            note = it.getString("NOTE") ?: "?"
         }
     }
 
 
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
         _binding = FragmentBlackKeyBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        if (note != null) {
-            view.blackKey.setOnTouchListener { _, event ->
+        view.whiteKey.setOnTouchListener(object: View.OnTouchListener{
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
                 when (event?.action) {
-                    MotionEvent.ACTION_DOWN -> this@BlackKeyFragment.onKeyDown.invoke(note!!)
-                    MotionEvent.ACTION_UP -> this@BlackKeyFragment.onKeyUp.invoke(note!!)
+                    MotionEvent.ACTION_DOWN -> {this@BlackKeyFragment.onKeyDown.invoke(note)}
+                    MotionEvent.ACTION_UP -> {this@BlackKeyFragment.onKeyUp.invoke(note)}
                 }
-                true
+                return true
             }
-        }
+        })
         return view
     }
 
--- piano/app/src/main/java/com/example/piano/WhiteKeyFragment.kt
+++ piano/app/src/main/java/com/example/piano/WhiteKeyFragment.kt
@@ -13,7 +13,7 @@ import kotlinx.android.synthetic.main.fragment_white_key.view.*
 class WhiteKeyFragment : Fragment() {
     private lateinit var _binding: FragmentWhiteKeyBinding
     private val binding get() = _binding
-    private lateinit var note:String
+    private lateinit var note: String
 
     private val onKeyUp: (note:String) -> Unit = { println("Piano key Up $it") }
     private val onKeyDown: (note:String) -> Unit = { println("Piano key down $it") }
@@ -25,26 +25,31 @@ class WhiteKeyFragment : Fragment() {
         }
     }
 
-    @SuppressLint("ClickableViewAccessibility")
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentWhiteKeyBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        view.whiteKey.setOnTouchListener { _, event ->
-            when (event?.action) {
-                MotionEvent.ACTION_DOWN -> {this@WhiteKeyFragment.onKeyDown.invoke(note)}
-                MotionEvent.ACTION_UP -> {this@WhiteKeyFragment.onKeyUp.invoke(note)}
+        view.whiteKey.setOnTouchListener(object: View.OnTouchListener{
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                when (event?.action) {
+                    MotionEvent.ACTION_DOWN -> {this@WhiteKeyFragment.onKeyDown.invoke(note)}
+                    MotionEvent.ACTION_UP -> {this@WhiteKeyFragment.onKeyUp.invoke(note)}
+                }
+                return true
             }
-            true
-        }
+        })
         return view
     }
 
     companion object {
         @JvmStatic
         fun newInstance(note: String) =
-                WhiteKeyFragment().apply {
+            WhiteKeyFragment().apply {
                 arguments = Bundle().apply {
                     putString("NOTE", note)
                 }
